b193e22c54bc34cab80e38dbd54302467be981de
Hong Minhee <hong.minhee@planetariumhq.com>
2019-07-10 15:23:38 +0900

    Release note

--- release.sh
+++ release.sh
@@ -15,16 +15,30 @@ wget -O /tmp/github-release.tar.bz2 \
 tar xvfj /tmp/github-release.tar.bz2 -C /tmp
 rm /tmp/github-release.tar.bz2
 
+wget -O /tmp/submark \
+  https://github.com/dahlia/submark/releases/download/0.2.0/submark-linux-x86_64
+chmod +x /tmp/submark
+
 github_user="${GITHUB_REPOSITORY%/*}"
 github_repo="${GITHUB_REPOSITORY#*/}"
 
 alias github-release=/tmp/bin/linux/amd64/github-release
+alias submark=/tmp/submark
+
+submark \
+  -o /tmp/release-note.txt \
+  -iO \
+  --h2 "Version $tag" \
+  CHANGES.md
+
+cat /tmp/release-note.txt
 
 github-release release \
   --user "$github_user" \
   --repo "$github_repo" \
   --tag "$tag" \
-  --name "xunit-unity-runner $tag" || true
+  --name "xunit-unity-runner $tag" \
+  --description - < /tmp/release-note.txt || true
 
 ls -alh Dist
 
